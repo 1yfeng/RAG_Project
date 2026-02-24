9bb474b14fd6194df47671bfe29fe6b06d908b72
Yufeng Li <yufengli@microsoft.com>
2026-02-24 13:34:51 +0800

    add requirements and run docker; support 2 network connect db

--- app/db/database_postgres.py
+++ app/db/database_postgres.py
@@ -1,12 +1,13 @@
+import os
 import psycopg
 from psycopg.rows import dict_row
 
 async def get_connection():
     conn = await psycopg.AsyncConnection.connect(
-        host= "localhost",
-        dbname = "rag_db",
-        user= "postgres",
-        password= "postgres",
+        host= os.getenv("DB_HOST", "localhost"),
+        dbname = os.getenv("DB_NAME", "rag_db"),
+        user= os.getenv("DB_USER", "postgres"),
+        password= os.getenv("DB_PASSWORD", "postgres"),
         row_factory =dict_row
     )
     return conn
